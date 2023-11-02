04cc448406775d963b4fdcdbeacc0ab6b4110e49
Евгений Наумов <enaumov@mobiledep.ru>
2023-11-02 20:15:07 +0300

    Перетаскивание точек линий

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const canvasElements = []
+const canvasDots = []
 let currentAction = null
 
 const canvas = document.querySelector('#canvas')
@@ -8,16 +9,23 @@ const addLineBtn = document.querySelector('#addLine')
 class LineDot {
   _x = 0
   _y = 0
+  _listeners = {
+    change: []
+  }
 
-  constructor (target) {
-    this.target = target
+  constructor () {
     this.el = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
     this.el.classList.add('dot')
+    canvasDots.push(this)
   }
 
   set x (val) {
     this._x = val
     this.el.setAttribute('cx', val)
+
+    this._listeners.change.forEach(listener => {
+      listener(this)
+    })
   }
   get x() {
     return this._x
@@ -25,15 +33,48 @@ class LineDot {
   set y (val) {
     this._y = val
     this.el.setAttribute('cy', val)
+
+    this._listeners.change.forEach(listener => {
+      listener(this)
+    })
   }
   get y() {
     return this._y
+  }
+  addEventListener(type, listener) {
+    this._listeners[type].push(listener)
+  }
+  removeEventListener(type, listener) {
+    const index = this._listeners[type].findIndex(l => listener === l)
+    this._listeners[type].splice(index, 1);
   }
   append() {
     canvas.append(this.el)
   }
   remove() {
     this.el.remove()
+  }
+  activeMovable () {
+    this._moveListener = (e) => {
+      this.x = e.offsetX
+      this.y = e.offsetY
+    }
+
+    this._startListener = () => {
+      this.el.addEventListener('mousemove', this._moveListener)
+    }
+    this._endListener = () => {
+      this.el.removeEventListener('mousemove', this._moveListener)
+    }
+
+    this.el.addEventListener('mousedown', this._startListener)
+    this.el.addEventListener('mouseup', this._endListener)
+    this.el.addEventListener('mouseout', this._endListener)
+  }
+  deactivateMovable () {
+    this.el.removeEventListener('mousedown', this._startListener)
+    this.el.removeEventListener('mouseup', this._endListener)
+    this.el.removeEventListener('mouseout', this._endListener)
   }
 }
 
@@ -47,6 +88,17 @@ class Line {
     this.el = document.createElementNS("http://www.w3.org/2000/svg", 'line')
     this.dot1 = new LineDot()
     this.dot2 = new LineDot()
+    canvasElements.push(this)
+
+    this.dot1.addEventListener('change', (e) => {
+      if (this.x1 !== e.x) this.x1 = e.x
+      if (this.y1 !== e.y) this.y1 = e.y
+    })
+
+    this.dot2.addEventListener('change', (e) => {
+      if (this.x2 !== e.x) this.x2 = e.x
+      if (this.y2 !== e.y) this.y2 = e.y
+    })
   }
 
   set x1(val) {
@@ -98,54 +150,59 @@ class Line {
 const actions = {
   editing: {
     btnEl: addEditBtn,
-    start () {},
-    end () {}
+    start () {
+      canvasDots.forEach(dot => {
+        dot.activeMovable()
+      })
+    },
+    end () {
+      canvasDots.forEach(dot => {
+        dot.deactivateMovable()
+      })
+    }
   },
   addLine: {
     _currentLine: null,
     _clickCount: 0,
-    _moveListener(e) {
-      if (this._currentLine) {
-        this._currentLine.x2 = e.offsetX
-        this._currentLine.y2 = e.offsetY
-      }
-    },
-    _addLineClickListener (e) {
-      this._clickCount++
-      switch (this._clickCount) {
-        case 1:
-          this._currentLine = new Line()
-          this._currentLine.x1 = e.offsetX
-          this._currentLine.y1 = e.offsetY
-          this._currentLine.x2 = e.offsetX
-          this._currentLine.y2 = e.offsetY
-          this._currentLine.append()
-          canvas.addEventListener('mousemove', this._moveListenerBind)
-          break;
-        case 2:
-          if (this._currentLine) {
-            this._currentLine.x2 = e.offsetX
-            this._currentLine.y2 = e.offsetY
-            this._clickCount = 0
-            canvas.removeEventListener('mousemove', this._moveListenerBind)
-            canvasElements.push(this._currentLine)
-            this._currentLine = null
-          }
-          break;
-      }
-    },
     btnEl: addLineBtn,
     start() {
       this._currentLine = null
       this._clickCount = 0
-      this._moveListenerBind = this._moveListener.bind(this)
-      this._addLineClickListenerBind = this._addLineClickListener.bind(this)
-      canvas.addEventListener('click', this._addLineClickListenerBind)
+      const _moveListener = (e) => {
+        if (this._currentLine) {
+          this._currentLine.x2 = e.offsetX
+          this._currentLine.y2 = e.offsetY
+        }
+      }
+      this._addLineClickListener = (e) => {
+        this._clickCount++
+        switch (this._clickCount) {
+          case 1:
+            this._currentLine = new Line()
+            this._currentLine.x1 = e.offsetX
+            this._currentLine.y1 = e.offsetY
+            this._currentLine.x2 = e.offsetX
+            this._currentLine.y2 = e.offsetY
+            this._currentLine.append()
+            canvas.addEventListener('mousemove', _moveListener)
+            break;
+          case 2:
+            if (this._currentLine) {
+              this._currentLine.x2 = e.offsetX
+              this._currentLine.y2 = e.offsetY
+              this._clickCount = 0
+              canvas.removeEventListener('mousemove', _moveListener)
+              this._currentLine = null
+            }
+            break;
+        }
+      }
+      canvas.addEventListener('click', this._addLineClickListener)
     },
     end() {
       this._currentLine = null
       this._clickCount = 0
-      canvas.removeEventListener('click', this._addLineClickListenerBind)
+      canvas.removeEventListener('click', this._addLineClickListener)
     }
   }
 }
